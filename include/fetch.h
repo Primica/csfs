038db14160f63461311309e365d3fca31f6cde0b
Primica <arthur.malfere@gmail.com>
2025-12-19 14:36:32 +0100

    refactor: Clean up comments in fetch.h for clarity and conciseness

--- include/fetch.h
+++ include/fetch.h
@@ -11,13 +11,10 @@ typedef struct {
     FetchPrinter printer;
 } FetchModule;
 
-// Print all or a selection of modules (names in `only`, or all if only_count==0)
 int fetch_print(Shell *shell, const char **only, int only_count, int color_enabled);
 
-// List available modules on stdout
 void fetch_list_modules(void);
 
-// Number of available modules
 size_t fetch_count_modules(void);
 
 #endif // FETCH_H
